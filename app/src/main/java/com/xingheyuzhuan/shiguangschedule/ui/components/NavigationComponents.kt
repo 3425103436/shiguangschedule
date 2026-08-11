c116c99c0f320f5f3154fecd65f9b7d0c4c4154d
3425103436 <3425103436@qq.com>
2026-08-11 12:12:05 +0800

    fix: add padding import for floating navigation

--- app/src/main/java/com/xingheyuzhuan/shiguangschedule/ui/components/NavigationComponents.kt
+++ app/src/main/java/com/xingheyuzhuan/shiguangschedule/ui/components/NavigationComponents.kt
@@ -1,6 +1,7 @@
 package com.xingheyuzhuan.shiguangschedule.ui.components
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
